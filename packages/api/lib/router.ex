fcfb959d4d773e0842ff8b7ffa11a3f179df7bcb
Evgeniy Troynov <troinov174@gmail.com>
2019-02-01 09:16:31 +0500

    #master. impl to json method

--- packages/api/lib/router.ex
+++ packages/api/lib/router.ex
@@ -1,16 +1,22 @@
 defmodule Api.Router do
   use Plug.Router
 
+  defp json(data) do
+    data
+      |> Enum.to_list
+      |> Poison.encode!
+  end
+
   plug(:match)
   plug(:dispatch)
 
   get "/" do
     data = Mongo.find(:mongo, "organizations", %{}, limit: 5, pool: DBConnection.Poolboy)
-      |> Enum.to_list
-      |> Poison.encode!
+      |> json
 
     send_resp(conn, 200, data)
   end
+
 
   match(_, do: send_resp(conn, 404, "Oops!"))
 end
