9cdbf352730083f8cce6de03466a4d2e0c6c03e0
etroynov <troinof@gmail.com>
2021-01-04 17:14:40 +0300

    chore(*): update about info

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,12 @@
 export default () => (
   <div>
-    <h1>Blog</h1>
+    <h1>Eugene Troynov</h1>
+    <p>I'm front-end developer with 10+ years expirience. Currently i work for a company arrival.com</p>
+    <h2>Contacts</h2>
+    <ul>
+      <li>phone: <a href="tel:+79634658198"></a> +7 (963) 465 81 98</li>
+      <li>email: <a href="mailto:troinof@gmail.com">troinof@gmail.com</a></li>
+      <li>github: <a href="https://github.com/etroynov">etroynov</a></li>
+    </ul>
   </div>
 );
